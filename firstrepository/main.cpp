1567a1ad55958c9413b8bb34397ae4e32cd4f6d9
jackgaul <jack.tone.gaul@gmail.com>
2019-11-22 11:20:48 -0800

    Initial Commit

---
+++ firstrepository/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstrepository
+//
+//  Created by Jack Gaul on 11/22/19.
+//  Copyright © 2019 Jack Gaul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
